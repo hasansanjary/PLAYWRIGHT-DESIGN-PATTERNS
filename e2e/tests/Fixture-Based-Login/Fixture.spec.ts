592857a45828aca9706f6596255126d5dd8d6104
qartist <40137734+qartist@users.noreply.github.com>
2026-02-09 23:33:32 -0500

    playwright=cli-select package integration

--- e2e/tests/Fixture-Based-Login/Fixture.spec.ts
+++ e2e/tests/Fixture-Based-Login/Fixture.spec.ts
@@ -4,7 +4,7 @@ import { loginData } from './LoginData';
 
 test.describe('Fixture-based login', () => {
 
-    test('successful login with invalid credentials using Fixture', async ({ loginPage, page }) => {
+    test('@smoke successful login with invalid credentials using Fixture', async ({ loginPage, page }) => {
 
         //Logging in using POM class
 
