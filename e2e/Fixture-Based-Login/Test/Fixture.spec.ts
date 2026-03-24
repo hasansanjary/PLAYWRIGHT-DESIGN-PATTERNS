2ae2cdd29ec3d03d041e4d9fe909867bfe81cc38
qartist <40137734+qartist@users.noreply.github.com>
2026-03-24 16:55:06 -0400

    new staogeState concept testing

---
+++ e2e/Fixture-Based-Login/Test/Fixture.spec.ts
@@ -0,0 +1,95 @@
+import { test, expect } from '../Fixtures/Fixture';
+import { loginData } from '../TestData/LoginData';
+import { AppNavigator } from '../AppNavigation/AppNavigator';
+
+
+test.describe('Fixture-based login', () => {
+
+    test.skip('@smoke unsuccessful login with invalid credentials using Fixture', async ({ loginPage, page }) => {
+
+
+        //Logging in using POM clas
+
+        const Page = loginPage;
+        // await Page.navigateToLoginPage('https://parabank.parasoft.com/parabank/index.htm');
+        await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+        await Page.invalidLogin();
+
+        await page.waitForLoadState('load');
+        await expect(Page.internalErrorMessage).toHaveText('The username and password could not be verified.');
+
+
+
+    });
+
+    test.skip('@smoke successfull login with valid credentials using Fixture', async ({ loginPage, page }) => {
+
+
+        //Logging in using POM clas
+
+        const Page = loginPage;
+        await Page.navigateToLoginPage('https://parabank.parasoft.com/parabank/index.htm');
+        await Page.login();
+
+        await expect(page).toHaveTitle("ParaBank | Accounts Overview");
+
+
+    });
+
+
+    test.skip('@regression Login with Empty Fields shows validation and does not log in', async ({ loginPage }) => {
+
+        const Page = loginPage;
+        // Do not fill any fields
+        await Page.navigateToLoginPage('https://parabank.parasoft.com/parabank/index.htm');
+        await Page.clickLoginButton(); // Assumes you have a method to click login
+
+        // Check for validation messages (adjust selectors/text as needed)
+        // await expect(page.getByText('Please enter a username and password.')).toBeVisible();
+        await expect(Page.emptyLoginFieldValidation).toHaveText('Please enter a username and password.');
+
+        // Optionally, check that user is not logged in (e.g., login form still visible)
+        await expect(Page.loginForm).toBeVisible();
+    });
+
+    test('@regression Dashboard shows successful login', async ({ loginPage, dashboardPage, page }) => {
+
+        const Page = loginPage;
+        await Page.navigateToLoginPage('https://parabank.parasoft.com/parabank/index.htm');
+        await Page.login();
+
+        await page.context().storageState({ path: 'auth.json' });
+
+        await dashboardPage.page.waitForLoadState('load');
+        await expect(page).toHaveTitle("ParaBank | Accounts Overview");
+
+
+    });
+
+      test('@regression Dashboard shows successful logout', async ({ loginPage, dashboardPage, appNavigator, page }) => {
+
+        // const Page = loginPage;
+        // await Page.navigateToLoginPage('https://parabank.parasoft.com/parabank/index.htm');
+        // await Page.login();
+        test.use({ storageState: 'auth.json' });
+        
+        const nav = appNavigator;
+        await nav.goTo('dashboard');
+
+        await dashboardPage.page.waitForLoadState('load');
+        await expect(page).toHaveTitle("ParaBank | Accounts Overview");
+
+        await page.waitForTimeout(5000);
+        await dashboardPage.logOut();
+
+        await page.waitForTimeout(5000);
+        
+        await expect(page).toHaveTitle("ParaBank | Welcome | Online Banking");
+
+
+    });
+
+
+
+});
+
---
+++ e2e/Fixture-Based-Login/Test/storageState.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '../Fixtures/Fixture';
+
+
+test('@regression Dashboard shows successful login', async ({ loginPage, dashboardPage, page }) => {
+
+        const Page = loginPage;
+        await Page.navigateToLoginPage('https://parabank.parasoft.com/parabank/index.htm');
+        await Page.login();
+
+        await page.context().storageState({ path: 'auth.json' });
+
+        await dashboardPage.page.waitForLoadState('load');
+        await expect(page).toHaveTitle("ParaBank | Accounts Overview");
+
+
+    });
+
+      test('@regression Dashboard shows successful logout', async ({ dashboardPage, appNavigator, page }) => {
+
+        test.use({ storageState: 'auth.json' });
+        
+        const nav = appNavigator;
+        await nav.goTo('dashboard');
+
+        await dashboardPage.page.waitForLoadState('load');
+        await expect(page).toHaveTitle("ParaBank | Accounts Overview");
+
+        await page.waitForTimeout(5000);
+        await dashboardPage.logOut();
+
+        await page.waitForTimeout(5000);
+        
+        await expect(page).toHaveTitle("ParaBank | Welcome | Online Banking");
+
+
+    });
