8752912b107cfa6facf8e5667569d91fdd2b4cda
qartist <40137734+qartist@users.noreply.github.com>
2026-03-24 15:03:59 -0400

    new dynamic navigation and asserting module

---
+++ e2e/Fixture-Based-Login/AppNavigation/AppNavigator.ts
@@ -0,0 +1,28 @@
+import { Page } from '@playwright/test';
+
+type AppPage = 'login' | 'dashboard';
+
+const PAGE_URLS: Record<AppPage, string> = {
+  login: 'https://parabank.parasoft.com/parabank/index.htm',
+  dashboard: 'https://parabank.parasoft.com/parabank/overview.htm',
+};
+
+export class AppNavigator {
+  constructor(private page: Page) {}
+
+  async goTo(pageName: AppPage) {
+    await this.page.goto(PAGE_URLS[pageName]);
+    await this.page.waitForURL(`**${PAGE_URLS[pageName]}`);
+  }
+
+//   async isAt(pageName: AppPage): Promise<boolean> {
+//     const url = new URL(this.page.url());
+//     return url.pathname === PAGE_URLS[pageName];
+//   }
+
+  async isAt(pageName: AppPage): Promise<boolean> {
+  const currentUrl = new URL(this.page.url());
+  const expectedUrl = new URL(PAGE_URLS[pageName]);
+  return currentUrl.pathname === expectedUrl.pathname;
+}
+}
