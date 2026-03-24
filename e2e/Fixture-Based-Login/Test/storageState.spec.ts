c03f262d9507d7a860de48b136a2a41985cd2256
qartist <40137734+qartist@users.noreply.github.com>
2026-03-24 17:06:09 -0400

    changes to the storageState capabilities and fixes

--- e2e/Fixture-Based-Login/Test/storageState.spec.ts
+++ e2e/Fixture-Based-Login/Test/storageState.spec.ts
@@ -3,34 +3,34 @@ import { test, expect } from '../Fixtures/Fixture';
 
 test('@regression Dashboard shows successful login', async ({ loginPage, dashboardPage, page }) => {
 
-        const Page = loginPage;
-        await Page.navigateToLoginPage('https://parabank.parasoft.com/parabank/index.htm');
-        await Page.login();
+  const Page = loginPage;
+  await Page.navigateToLoginPage('https://parabank.parasoft.com/parabank/index.htm');
+  await Page.login();
 
-        await page.context().storageState({ path: 'auth.json' });
+  await page.context().storageState({ path: 'auth.json' });
 
-        await dashboardPage.page.waitForLoadState('load');
-        await expect(page).toHaveTitle("ParaBank | Accounts Overview");
-
-
-    });
-
-      test('@regression Dashboard shows successful logout', async ({ dashboardPage, appNavigator, page }) => {
-
-        test.use({ storageState: 'auth.json' });
-        
-        const nav = appNavigator;
-        await nav.goTo('dashboard');
-
-        await dashboardPage.page.waitForLoadState('load');
-        await expect(page).toHaveTitle("ParaBank | Accounts Overview");
-
-        await page.waitForTimeout(5000);
-        await dashboardPage.logOut();
-
-        await page.waitForTimeout(5000);
-        
-        await expect(page).toHaveTitle("ParaBank | Welcome | Online Banking");
+  await dashboardPage.page.waitForLoadState('load');
+  await expect(page).toHaveTitle("ParaBank | Accounts Overview");
 
 
-    });
+});
+
+test.use({ storageState: 'auth.json' });
+
+test('@regression Dashboard shows successful logout', async ({ dashboardPage, appNavigator, page }) => {
+
+  const nav = appNavigator;
+  await nav.goTo('dashboard');
+
+  await dashboardPage.page.waitForLoadState('load');
+  await expect(page).toHaveTitle("ParaBank | Accounts Overview");
+
+  await page.waitForTimeout(5000);
+  await dashboardPage.logOut();
+
+  await page.waitForTimeout(5000);
+
+  await expect(page).toHaveTitle("ParaBank | Welcome | Online Banking");
+
+
+});
